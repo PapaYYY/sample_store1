da17c0cf658d5e53fba37b59c68c78036b52a862
Mykola Mamaiev <mamaievm@ae.com>
2025-04-08 14:37:23 +0300

    Init commit

---
+++ src/test/java/com/griddynamics/mamaievm/samplestoreapp/StoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griddynamics.mamaievm.samplestoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
