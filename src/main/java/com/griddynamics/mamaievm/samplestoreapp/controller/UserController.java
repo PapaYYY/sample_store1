7b567529da0cce4a6742b3cf158aee4050d5b570
Mykola Mamaiev <mamaievm@ae.com>
2025-10-01 19:16:51 +0300

    Update UserController

--- src/main/java/com/griddynamics/mamaievm/samplestoreapp/controller/UserController.java
+++ src/main/java/com/griddynamics/mamaievm/samplestoreapp/controller/UserController.java
@@ -2,15 +2,19 @@ package com.griddynamics.mamaievm.samplestoreapp.controller;
 
 import com.griddynamics.mamaievm.samplestoreapp.domain.UserLoginResponse;
 import com.griddynamics.mamaievm.samplestoreapp.dto.UserDto;
-import com.griddynamics.mamaievm.samplestoreapp.exception.WrongCredentialsException;
+import com.griddynamics.mamaievm.samplestoreapp.exception.BruteForcingBlockerException;
+import com.griddynamics.mamaievm.samplestoreapp.service.LoginAttemptService;
 import com.griddynamics.mamaievm.samplestoreapp.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +25,13 @@ import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping(UserController.BASE_URL)
 @RequiredArgsConstructor
@@ -37,7 +42,8 @@ public class UserController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final SecurityContextRepository securityContextRepository;
-
+    private final LoginAttemptService loginAttemptService;
+    
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     UserDto createUser(@RequestBody @NonNull @Valid UserDto userDTO) {
@@ -53,16 +59,28 @@ public class UserController {
 
     @PostMapping("/update")
     @ResponseStatus(HttpStatus.OK)
-    UserDto updateUser(@RequestBody @NonNull @Valid UserDto userDTO) {
+    UserDto updateUser(@RequestBody @NonNull @Valid UserDto userDTO, @AuthenticationPrincipal UserDetails userDetails) {
+        String currentUserName = userDetails.getUsername();
+        log.debug("Updating user {}", currentUserName);
+        if (!currentUserName.equals(userDTO.getEmail())) throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Please provide proper user email");
         if(userDTO.getId() == null) {
             throw new IllegalArgumentException("User id is required for user update");
         }
         return userService.saveUser(userDTO);
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    String getCurrentUserName(@AuthenticationPrincipal UserDetails userDetails) {
+        return userDetails.getUsername();
+    }
+
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
     UserLoginResponse login(@RequestBody @NonNull @Valid UserDto userDTO, HttpServletRequest request, HttpServletResponse response) {
+        if (loginAttemptService.isBlocked()) {
+            throw new BruteForcingBlockerException("IP is blocked");
+        }
         UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(userDTO.getEmail(), userDTO.getPassword());
         Authentication authentication = authenticationManager.authenticate(authRequest);
 
